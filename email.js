15e6c5f2acf16f5345f0721897d81644f586b80e
RinaPanu <113353473+RinaPanu@users.noreply.github.com>
2023-06-11 17:15:06 +0500

    Add files via upload

--- email.js
+++ email.js
@@ -1,42 +1,20 @@
-require('dotenv').config()
-const express = require('express');
 
-const router = express();
+$(document).ready(function() {
+  //E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
-router.post('/email', function (req, res) {
-    var transporter = require('nodemailer').createTransport({
-      service:'gmail',
-      port: 465,
-      secure: true,
-      auth: {
-        // SMTP-сервер, который будет отправлять письма
-        user: 'proektnyjp4638@gmail.com',
-        pass: 'xezxwduwbtptolaz'
-      }
-    });
-    var opt = {
-      from: req.body.from,
-      to: 'proektnyjp4638@gmail.com', // Почта сайта
-      subject: req.body.subject,
-      html: req.body.message
-    };
-    transporter.sendMail(opt, function(err) {
-      if (err) console.error(err);
-      res.send('Send Message!');
-    });
-  });
-// const transporter = nodemailer.createTransport({
-//     service:'gmail',
-//     auth: {
-//         user: 'proektnyjp4638@gmail.com',
-//         pass: 'xezxwduwbtptolaz'
-//     }
-// })
-
-// const mailOptions = {
-//     from: 'proektnyjp4638@gmail.com',
-//     to : 'proektnyjp4638@gmail.com',
-//     subject: 'Письмо черерз node.js',
-//     text: 'Текст самого пиьма'
-// }
-// transporter.sendMail(mailOptions)
+});
